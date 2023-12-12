c226d1e11840dd1d097e9c33a26b6039ac835378
SuPA514 <lacassemichel@gmail.com>
2023-12-12 13:57:27 -0500

    s

--- run.py
+++ run.py
@@ -49,21 +49,10 @@ def internet(host="8.8.8.8", port=53, timeout=3):
 #main
 print("------------------------------------")
 
-import platform,subproccess,re
-def Ping(hostname,timeout):
-    if platform.system() == "Windows":
-        command="ping "+hostname+" -n 1 -w "+str(timeout*1000)
-    else:
-        command="ping -i "+str(timeout)+" -c 1 " + hostname
-    proccess = subprocess.Popen(command, stdout=subprocess.PIPE)
-    matches=re.match('.*time=([0-9]+)ms.*', proccess.stdout.read(),re.DOTALL)
-    if matches:
-        return matches.group(1)
-    else: 
-        return False
+import platform
+import subprocess
 
-
-Ping("google.ca")
+#Ping("google.ca")
 #router_on()
 #time.sleep(1)
 #router_off()
