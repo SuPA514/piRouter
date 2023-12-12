f3074128c1f7a97d0d84c830e549d85f94c074fb
SuPA514 <lacassemichel@gmail.com>
2023-12-12 09:39:11 -0500

    d

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import RPi.GPIO as GPIO
 import time
 
+from ping3 import ping, verbose_ping
+
 
 
 
@@ -18,14 +20,31 @@ def router_off():
     GPIO.setup(2, GPIO.OUT)
     GPIO.output(2, GPIO.LOW)
 
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
 
 
-
+ping('google.ca')
 
 #main
 
-router_on()
-time.sleep(1)
-router_off()
-time.sleep(1)
-router_on()
+
+
+
+
+#router_on()
+#time.sleep(1)
+#router_off()
+#time.sleep(1)
+#router_on()
